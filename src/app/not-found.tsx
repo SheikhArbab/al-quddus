3c2777eee0a1c14f66c4e9d41baa511d1fde6b2d
sheikharbab <arbabzafar4444@gmail.com>
2024-03-11 00:23:36 +0500

    fix comma

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -12,7 +12,7 @@ export default function NotFound() {
                         Sorry we couldn't find this page.{" "}
                     </p>
                     <p className="mb-8">
-                        But dont worry, you can find plenty of other things on our homepage.
+                       {` But dont worry&#x2C; you can find plenty of other things on our homepage.`}
                     </p>
                     <Link href={'/'}>
                         <button className="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-blue-600 active:bg-blue-600 hover:bg-blue-700">
