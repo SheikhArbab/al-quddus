a5e68a33991762211d9fd2befb8406c00a1382bf
sheikharbab <arbabzafar4444@gmail.com>
2024-03-11 00:27:45 +0500

    fix comma issue

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -9,10 +9,10 @@ export default function NotFound() {
                 <div className="max-w-md">
                     <div className="text-5xl font-dark font-bold">404</div>
                     <p className="text-2xl md:text-3xl font-light leading-normal">
-                        Sorry we couldn't find this page.{" "}
+                       {` Sorry we couldn't find this page.`}{" "}
                     </p>
                     <p className="mb-8">
-                       {` But dont worry&#x2C; you can find plenty of other things on our homepage.`}
+                       {` But dont worry, you can find plenty of other things on our homepage.`}
                     </p>
                     <Link href={'/'}>
                         <button className="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-blue-600 active:bg-blue-600 hover:bg-blue-700">
